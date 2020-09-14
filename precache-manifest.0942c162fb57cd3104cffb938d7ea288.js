8c62319bbc3baf8572e79d87f2e19293aa626c9f
shanusaifi <mohdshanu2@gmail.com>
2020-09-14 11:51:12 +0530

    Updates

---
+++ precache-manifest.0942c162fb57cd3104cffb938d7ea288.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a154a3aa562c4ef752c773d5dcb4d723",
+    "url": "/facebook-messenger/index.html"
+  },
+  {
+    "revision": "7ad8321fdede7577190b",
+    "url": "/facebook-messenger/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d519802b1cf0b7f1ee90",
+    "url": "/facebook-messenger/static/js/2.047dd2e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facebook-messenger/static/js/2.047dd2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad8321fdede7577190b",
+    "url": "/facebook-messenger/static/js/main.f43b972b.chunk.js"
+  },
+  {
+    "revision": "f361b87a14f4c7d9ca49",
+    "url": "/facebook-messenger/static/js/runtime-main.9ee3e68e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/facebook-messenger/static/media/logo.5d5d9eef.svg"
+  }
+]);
